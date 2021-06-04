911a18018bae91c7162a8cca2e7d3c1c5c23cb5d
İbrahim Uzun <info@ibrahimuzun.com>
2021-06-04 21:13:13 +0300

    Some tailwind css layer components

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,10 @@ function HomePage() {
       setCount(parseInt(savedCount));
     }
   }, []);
-
   const onLongPress = () => {
     setCount(count + 1);
     localStorage.setItem("count", count + 1);
   };
-
   const onClick = () => {
     setCount(count + 1);
     localStorage.setItem("count", count + 1);  };
@@ -28,9 +26,9 @@ function HomePage() {
   const longPressEvent = useLongPress(onLongPress, onClick, defaultOptions);
 
   return (
-    <div className="flex justify-center items-center h-screen mx-auto background">
+    <div className="main">
       <button
-        className="flex flex-col items-center px-4 sm:px-8 py-5 text-4xl font-light text-white bg-black mt-[-64px] bg-opacity-20 backdrop-filter backdrop-blur firefox:bg-opacity-80 rounded-2xl transform scale-100 transition hover:scale-105 active:scale-115 focus:outline-none"
+        className="button"
         {...longPressEvent}
       >
         Sayıyı arttır {count}
